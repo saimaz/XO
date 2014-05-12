2f2db8311e358e6e520d2c861d7e84f61830bbe5
Audrius Bugas <audrius.bugas@nfq.com>
2014-05-12 19:58:00 +0300

    introduced strategy interface

---
+++ src/XO/StrategyInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace XO;
+
+/**
+ * This interface provides basic structure for game strategy
+ */
+interface StrategyInterface
+{
+    /**
+     * Possible symbols to use
+     */
+    const SYMBOL_X = 'X';
+    const SYMBOL_O = 'O';
+
+    /**
+     * @param array $table Array of current game state in 3x3 matrix
+     * @param string $symbol Symbol to use to do return
+     * @return array coordinates of new turn, fox example [0,1]
+     */
+    public function turn($table, $symbol = self::SYMBOL_X);
+}
